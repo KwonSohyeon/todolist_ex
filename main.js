9118889cc42e51b6dc8c7185ce66af52dbe76028
KwonSohyeon <gusl030725@gmail.com>
2025-02-10 11:57:34 +0900

    구조 및 제이쿼리 연결

---
+++ main.js
@@ -0,0 +1,8 @@
+const input = document.getElementById("input-text");
+const btn = document.getElementById("add");
+
+btn.addEventListener("click", () => {
+  let inputValue = document.getElementById("input-text").value;
+
+  let li = document.createElement("li");
+});
