93c2b702d91ac5c2cff5ec68f182a1539548b4c3
KwonSohyeon <gusl030725@gmail.com>
2025-02-11 12:51:19 +0900

    버튼 클릭 이벤트 시도

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ btn.addEventListener("click", () => {
 
   let li = document.createElement("li");
 });
+
+$(document).ready(function () {
+  $("#add").on("click", function () {
+    let list = $("li");
+    $("ul").append(list);
+  });
+});
+
+// Todo 객체 생성
+// 기본 속성 : 내용, 체크여부, id
+const todos = [todo, todo, todo];
+
+const todo = {
+  title: "",
+  checked: false,
+  id: "",
+};
