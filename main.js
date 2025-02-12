fd83d39502a164325ac81cd44a3ca372c700a11c
KwonSohyeon <gusl030725@gmail.com>
2025-02-12 16:32:04 +0900

    li 및 버튼 생성

--- main.js
+++ main.js
@@ -2,24 +2,22 @@ const input = document.getElementById("input-text");
 const btn = document.getElementById("add");
 
 btn.addEventListener("click", () => {
-  let inputValue = document.getElementById("input-text").value;
+  let ul = document.querySelector("ul");
+  let list = document.createElement("li");
+  let check = document.createElement("input");
+  check.setAttribute("type", "checkbox");
 
-  let li = document.createElement("li");
-});
-
-$(document).ready(function () {
-  $("#add").on("click", function () {
-    let list = $("li");
-    $("ul").append(list);
-  });
+  list.textContent = input.value;
+  list.appendChild(check);
+  ul.appendChild(list);
 });
 
 // Todo 객체 생성
-// 기본 속성 : 내용, 체크여부, id
-const todos = [todo, todo, todo];
+// Dummy Data(가짜데이터)
+const todos = [
+  { title: "자바스크립트 공부", content: "배열메서드", date: "오늘까지" },
+  { title: "자바스크립트 공부", content: "DOM", date: "내일까지" },
+];
 
-const todo = {
-  title: "",
-  checked: false,
-  id: "",
-};
+// 화면에 투두리스트 전체를 보여주는 함수
+function loadTodos() {}
